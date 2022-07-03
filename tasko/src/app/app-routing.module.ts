09ab9d602469928b2c76be94ff0bf8417c3a4ba1
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-03 11:16:37 -0500

    Add page not found route ✨

--- tasko/src/app/app-routing.module.ts
+++ tasko/src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'login',pathMatch :'full'},
   {path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path: '**',component:PageNotFoundComponent}
+
 ];
 
 @NgModule({
--- tasko/src/app/app.module.ts
+++ tasko/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
