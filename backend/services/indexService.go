4b80ad0b01ba36279b1548a8de8ec3fbd0726460
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-09 15:05:30 -0500

    Update index service ✨

--- backend/services/indexService.go
+++ backend/services/indexService.go
@@ -1,12 +1,21 @@
 package services
 
 import (
-	"fmt"
+	"backend/models/utils"
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func IndexService(writer http.ResponseWriter, request *http.Request, bodyBytes []byte) {
+
+	var response utils.GenericResponse
+
 	log.Printf("Index service accessed")
-	fmt.Fprint(writer, "Welcome!")
+
+	response.Response = "success"
+
+	writer.WriteHeader(http.StatusOK)
+	json.NewEncoder(writer).Encode(response)
+
 }
