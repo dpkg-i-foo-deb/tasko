1f7fc097716f72d66684ceb4d4c4c991c7be851a
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-08 20:00:33 -0500

    Add home to routing module 👷

--- tasko/src/app/app-routing.module.ts
+++ tasko/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 import { LoginComponent } from './views/login/login.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 import { SignupComponent } from './views/signup/signup.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   {path: '', redirectTo:'login',pathMatch :'full'},
   {path:'login',component:LoginComponent},
   {path:'signup',component:SignupComponent},
+  {path:'home',component:HomeComponent},
   {path: '**',component:PageNotFoundComponent}
 
 ];
