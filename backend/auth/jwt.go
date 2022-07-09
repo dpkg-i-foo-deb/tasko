862b9c08f5a4b0392bb691fbaf572ef311dbb411
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-09 10:43:51 -0500

    Use Http status 401 instead of 403 🔒

--- backend/auth/jwt.go
+++ backend/auth/jwt.go
@@ -124,7 +124,7 @@ func ValidateAndContinue(next func(writer http.ResponseWriter, request *http.Req
 		accessCookie, err = r.Cookie("access-token")
 
 		if err != nil {
-			w.WriteHeader(http.StatusForbidden)
+			w.WriteHeader(http.StatusUnauthorized)
 			log.Print("The request did not contain the access cookie")
 			response.Response = "The access cookie was not found"
 			json.NewEncoder(w).Encode(response)
