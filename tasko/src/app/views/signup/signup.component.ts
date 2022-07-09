86e5620a6f02e5a0c32bfd3cfcc023b19cb634b6
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-08 19:03:18 -0500

    Add initial signup form 🎉

--- tasko/src/app/views/signup/signup.component.ts
+++ tasko/src/app/views/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  signupForm = new FormGroup({
+    email : new FormControl('',[Validators.required, Validators.email]),
+    password : new FormControl ('',[Validators.required,Validators.minLength(4)]),
+    password_confirm : new FormControl('',[Validators.required,Validators.minLength(4)]),
+    first_name: new FormControl('',Validators.required),
+    last_name: new FormControl('')
+  })
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  signup(){
+
+  }
+
+  get email(){
+    return this.signupForm.get('email');
+  }
+
+  get password(){
+    return this.signupForm.get('password');
+  }
+
+  get first_name(){
+    return this.signupForm.get('first_name');
+  }
+
+  get last_name(){
+    return this.signupForm.get('last_name');
+  }
+
+  get password_confirm(){
+    return this.signupForm.get('password_confirm');
   }
 
 }
