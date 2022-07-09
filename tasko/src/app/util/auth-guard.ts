7ade3bd590121de9292b88f0e57f882be8306dfd
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-09 10:08:39 -0500

    Fix ugly auth guard name 🛡️

--- tasko/src/app/util/auth-guard.ts
+++ tasko/src/app/util/auth-guard.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardGuard implements CanActivate {
+export class AuthGuard implements CanActivate {
+
+
+  constructor (private cookieService:CookieService, private router:Router){}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
