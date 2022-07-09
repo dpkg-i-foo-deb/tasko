b8e10d6c1bf68d7fb32ede9ade542aaf4d87320d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-09 10:13:58 -0500

    Protect the home route! 🛡️

--- tasko/src/app/util/auth-guard.ts
+++ tasko/src/app/util/auth-guard.ts
@@ -11,10 +11,24 @@ export class AuthGuard implements CanActivate {
 
   constructor (private cookieService:CookieService, private router:Router){}
 
+  shouldRedirect (cookieExists:boolean):any{
+    if(!cookieExists)
+    {
+      this.router.navigate(['login']);
+    }
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+    //Step 1, verify is the access token exists
+    const cookieExists = this.cookieService.check('access-token');
+
+    //If it does not exist, redirect the user to the login page
+    this.shouldRedirect(cookieExists)
+
+    return cookieExists;
   }
   
 }
