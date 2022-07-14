7695d8ab697bf879ae01d9d1f46d25b5d94ee086
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-14 10:37:54 -0500

    Tidy up ♻️

--- tasko/src/app/app.module.ts
+++ tasko/src/app/app.module.ts
@@ -1,21 +1,30 @@
+//Angular Stuffies
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
+//Component stuffies
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { SignupComponent } from './views/signup/signup.component';
 import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { HomeComponent } from './views/home/home.component'
 
+//Forms and HTTP stuffies
 import {ReactiveFormsModule,FormsModule} from '@angular/forms'
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+//Material Stuffies
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatInputModule} from '@angular/material/input'
 import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar'
+
+//Other stuffies
 import { HotToastModule } from '@ngneat/hot-toast';
-import { HomeComponent } from './views/home/home.component'
 import { AuthInterceptor } from './interceptors/auth-interceptor';
+
 
 @NgModule({
   declarations: [
@@ -35,6 +44,7 @@ import { AuthInterceptor } from './interceptors/auth-interceptor';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatToolbarModule,
     HotToastModule.forRoot()
   ],
 
