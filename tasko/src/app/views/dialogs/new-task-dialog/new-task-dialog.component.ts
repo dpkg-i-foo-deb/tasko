ee097d861f8fdaeece23666efe2ead128303940d
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-23 17:25:32 -0500

    Close task form when created! ✨

--- tasko/src/app/views/dialogs/new-task-dialog/new-task-dialog.component.ts
+++ tasko/src/app/views/dialogs/new-task-dialog/new-task-dialog.component.ts
@@ -1,62 +1,68 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import {Api} from 'src/app/api/api';
-import { Task } from '../../../models/task'
+import { MatDialogRef } from '@angular/material/dialog';
+import { HotToastService } from '@ngneat/hot-toast';
+import { Api } from 'src/app/api/api';
+import { Task } from '../../../models/task';
 
 @Component({
   selector: 'app-new-task-dialog',
   templateUrl: './new-task-dialog.component.html',
-  styleUrls: ['./new-task-dialog.component.scss']
+  styleUrls: ['./new-task-dialog.component.scss'],
 })
 export class NewTaskDialogComponent implements OnInit {
-
   newTaskForm = new FormGroup({
     title: new FormControl('', [Validators.required]),
     description: new FormControl(''),
     startDate: new FormControl('', [Validators.required]),
-    dueDate: new FormControl('', [Validators.required])
-  })
+    dueDate: new FormControl('', [Validators.required]),
+  });
 
-  constructor(private api:Api) { }
+  constructor(
+    private api: Api,
+    private toast: HotToastService,
+    private dialogRef: MatDialogRef<NewTaskDialogComponent>
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   createTask() {
-	let task: Task = {
-	code:0,
-	title: '',
-	description: '',
-	start_date:'',
-	due_date:'',
-	status:false,
-	user_email:'',
-	}
+    let task: Task = {
+      code: 0,
+      title: '',
+      description: '',
+      start_date: '',
+      due_date: '',
+      status: false,
+      user_email: '',
+    };
 
-	task.title= this.newTaskForm.controls['title'].value ?? ''
-	task.description = this.newTaskForm.controls['description'].value ?? ''
-	task.start_date = this.newTaskForm.controls['startDate'].value ?? ''
-	task.due_date = this.newTaskForm.controls['dueDate'].value ?? ''
+    task.title = this.newTaskForm.controls['title'].value ?? '';
+    task.description = this.newTaskForm.controls['description'].value ?? '';
+    task.start_date = this.newTaskForm.controls['startDate'].value ?? '';
+    task.due_date = this.newTaskForm.controls['dueDate'].value ?? '';
 
-	console.log('oli')
+    console.log('oli');
 
-	this.api.createTask(task).pipe()
-	.subscribe(
-		{
-			next:()=>{
+    this.api
+      .createTask(task)
+      .pipe(
+        this.toast.observe({
+          success: 'Task Created Successfully!',
+          loading: 'Creating new Task...',
+          error: 'Failed!',
+        })
+      )
+      .subscribe({
+        next: () => {},
 
-			},
+        error: () => {
+          this.toast.show('Could not Create the Task!');
+        },
 
-			error: (error:any) => {
-				console.log(error)
-			},
-
-			complete:() => {
-
-			}
-		}
-	)	
-
+        complete: () => {
+          this.dialogRef.close();
+        },
+      });
   }
-
 }
--- tasko/src/app/views/login/login.component.ts
+++ tasko/src/app/views/login/login.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { HotToastService } from '@ngneat/hot-toast'
+import { HotToastService } from '@ngneat/hot-toast';
 
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
-import { FormGroup, FormControl, Validators } from '@angular/forms'
+import { User } from '../../models/user';
 
-import { User } from '../../models/user'
-
-import { Api } from '../../api/api'
+import { Api } from '../../api/api';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
@@ -14,27 +13,27 @@ import { CookieService } from 'ngx-cookie-service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [Validators.required, Validators.minLength(4)])
-  })
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(4),
+    ]),
+  });
 
   constructor(
     private api: Api,
     private toast: HotToastService,
     private router: Router,
-    private cookieService: CookieService,
-  ) { }
+    private cookieService: CookieService
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-
     if (!this.loginForm.valid) {
       return;
     }
@@ -49,49 +48,43 @@ export class LoginComponent implements OnInit {
     user.email = this.loginForm.controls['email'].value ?? '';
     user.password = this.loginForm.controls['password'].value ?? '';
 
-    this.api.login(user).pipe(
-      this.toast.observe({
-        success: 'Logged in Successfully!',
-        loading: 'Logging in...',
-        error: 'Something Went Wrong. Try Again!'
-      })
-    ).subscribe(
-      {
+    this.api
+      .login(user)
+      .pipe(
+        this.toast.observe({
+          success: 'Logged in Successfully!',
+          loading: 'Logging in...',
+          error: 'Something Went Wrong. Try Again!',
+        })
+      )
+      .subscribe({
         next: () => {
           //TODO what the heck do I do here?
         },
 
         error: (error: HttpErrorResponse) => {
-
           //401 = Username or password incorrect
           if (error.status == 401) {
-            this.toast.show(
-              "Username or Password Incorrect!",
-            )
+            this.toast.show('Username or Password Incorrect!');
           }
           //Other stuff means backend is not running
           else {
-            this.toast.show(
-              "Cannot Connect to Backend! Is it Running?"
-            ),
-            {
-              duration: 15,
-              autoClose: false,
-              dismissible: true,
-            }
+            this.toast.show('Cannot Connect to Backend! Is it Running?'),
+              {
+                duration: 15,
+                autoClose: false,
+                dismissible: true,
+              };
           }
         },
 
         complete: () => {
-
           //Create a session cookie
-          this.cookieService.set('active-session', 'true')
+          this.cookieService.set('active-session', 'true');
 
           this.router.navigate(['/dashboard']);
-        }
-      }
-    );
-
+        },
+      });
   }
 
   get email() {
@@ -101,5 +94,4 @@ export class LoginComponent implements OnInit {
   get password() {
     return this.loginForm.get('password');
   }
-
 }
