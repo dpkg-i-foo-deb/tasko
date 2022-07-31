b6382dfefb625da68d1f3d027eee152759b39064
Mateo Estrada Ramirez <mateoestradaramirez@outlook.com>
2022-07-30 20:37:41 -0500

    Initial fake prod variables ➕

--- tasko/src/environments/environment.prod.ts
+++ tasko/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:3000/',
+  loginPath: 'login',
+  signupPath: 'sign-up',
+  pingPath: '',
+  refreshPath: 'refresh',
+  getTasksPath: 'tasks',
+  createTaskPath: 'tasks',
+  updateTaskPath: 'tasks',
+  deleteTaskPath: 'tasks',
+  signOutPath: 'sign-out',
 };
